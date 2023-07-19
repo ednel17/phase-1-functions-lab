6a7e16ba7fd5701550ffcaf85a39cb62aa809926
Carl <carljeanpierre@gmail.com>
2023-07-18 20:48:03 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    return Math.abs(blockNumber - 42);
+};
+
+function distanceFromHqInFeet(blockInFeet){
+    return (distanceFromHqInBlocks(blockInFeet)*264);
+};
+
+function distanceTravelledInFeet(start, end){
+    const blocksTravelled = Math.abs(end - start)
+    return blocksTravelled * 264;
+};
+
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet(start, end);
+
+    if (distance <= 400){
+        return 0;
+    } else if (distance > 400 && distance <= 2000){
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
